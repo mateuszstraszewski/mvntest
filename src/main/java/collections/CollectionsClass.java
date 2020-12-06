af9eedba951bd251451587375323234dd0d123f2
mateuszstraszewski <strachoo@gmail.com>
2020-12-06 10:37:48 +0100

    Added Main file, initialized Lists in Collections file.

--- src/main/java/collections/CollectionsClass.java
+++ src/main/java/collections/CollectionsClass.java
@@ -1,7 +1,25 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CollectionsClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world, it is a Maven project.");
+    public static void trainList() {
+        String name1 = "John";
+        String name2 = "Mark";
+        String name3 = "Glenn";
+        List<String> nameList= new LinkedList<>();
+//        ArrayList<String> nameList= new ArrayList<>();
+
+        nameList.add(name1);
+        nameList.add(name2);
+        nameList.add(name3);
+
+        System.out.println(nameList.size());
+
+        nameList.add(name2);
+
+        System.out.println(nameList.size());
     }
 }
